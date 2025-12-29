22abe7a790969c3ccc494882a51364b7a1e32fb4
Fardin Mohammed <fardeenmohdpl@gmail.com>
2025-12-30 02:30:00 +0530

    Add response entity and status code to DELETE category API

--- src/main/java/com/webstore/implementation/CategoryServiceImplementation.java
+++ src/main/java/com/webstore/implementation/CategoryServiceImplementation.java
@@ -2,7 +2,9 @@ package com.webstore.implementation;
 
 import com.webstore.model.Category;
 import com.webstore.service.CategoryService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +27,11 @@ public class CategoryServiceImplementation implements CategoryService {
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
-        if(category == null) {
-            return "Category with categoryId: " + categoryId + " not found";
-        }
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " has been deleted";
 
