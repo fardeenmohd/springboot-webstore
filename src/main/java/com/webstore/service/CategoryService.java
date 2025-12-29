6ef2712333c29839b0f4ec6266bb772500adfd50
Fardin Mohammed <fardeenmohdpl@gmail.com>
2025-12-30 01:59:50 +0530

    Add controller, service and implementation for categories

---
+++ src/main/java/com/webstore/service/CategoryService.java
@@ -0,0 +1,10 @@
+package com.webstore.service;
+
+import com.webstore.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+}
