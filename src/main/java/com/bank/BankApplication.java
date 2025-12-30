616597a1630293ec9fc49f589748bd9a68b2748c
Fardin Mohammed <fardeenmohdpl@gmail.com>
2025-12-30 18:07:07 +0530

    Add a separate bank springboot application

---
+++ src/main/java/com/bank/BankApplication.java
@@ -0,0 +1,14 @@
+package com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collections;
+
+@SpringBootApplication
+public class BankApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BankApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+        app.run(args);
+    }
+}
