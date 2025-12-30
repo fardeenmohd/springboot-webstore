bb831433e864d67264f94b5553ed53538cd22641
Fardin Mohammed <fardeenmohdpl@gmail.com>
2025-12-30 18:11:09 +0530

    Add separate application.properties for bank and webstore app

--- src/main/java/com/bank/BankApplication.java
+++ src/main/java/com/bank/BankApplication.java
@@ -2,13 +2,12 @@ package com.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Collections;
 
 @SpringBootApplication
 public class BankApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(BankApplication.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+        app.setAdditionalProfiles("bank");
         app.run(args);
     }
 }
--- src/main/java/com/webstore/WebstoreApplication.java
+++ src/main/java/com/webstore/WebstoreApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebstoreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WebstoreApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(WebstoreApplication.class);
+        app.setAdditionalProfiles("webstore");
+        app.run(args);
+    }
 }
