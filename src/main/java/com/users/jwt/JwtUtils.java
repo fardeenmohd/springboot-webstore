3a5620fc78ec535d01c04d9804dbb5906584af1a
Fardin Mohammed <fardeenmohdpl@gmail.com>
2026-01-03 20:18:49 +0530

    Add authentication entry point for jwt

--- src/main/java/com/users/jwt/JwtUtils.java
+++ src/main/java/com/users/jwt/JwtUtils.java
@@ -22,7 +22,7 @@ public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
     @Value("${spring.app.jwtExpirationMs}")
-    private int jwtExpirationMs;
+    private long jwtExpirationMs;
     @Value("${spring.app.jwtSecret}")
     private String jwtSecret;
 
