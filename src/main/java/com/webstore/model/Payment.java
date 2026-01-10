76f65f51f51e1eb38e09fcdfba150e083bd89261
Fardin Mohammed <fardeenmohdpl@gmail.com>
2026-01-11 00:31:01 +0530

    Add fields for Payment model

--- src/main/java/com/webstore/model/Payment.java
+++ src/main/java/com/webstore/model/Payment.java
@@ -1,6 +1,8 @@
 package com.webstore.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,8 +17,23 @@ public class Payment {
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "order_id")
+    @OneToOne(mappedBy = "payment", cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     private Order order;
-    
+
+    @NotBlank
+    @Size(min = 4, message = "Payment method must contain at least 4 characters")
+    private String paymentMethod;
+
+    private String pgPaymentId;
+    private String pgStatus;
+    private String pgResponseMessage;
+    private String pgName;
+
+    public Payment(Long paymentId, String pgPaymentId, String pgStatus, String pgResponseMessage, String pgName) {
+        this.id = paymentId;
+        this.pgPaymentId = pgPaymentId;
+        this.pgStatus = pgStatus;
+        this.pgResponseMessage = pgResponseMessage;
+        this.pgName = pgName;
+    }
 }
