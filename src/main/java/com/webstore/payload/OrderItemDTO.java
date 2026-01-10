10b5216f1f8d043e4cef34cb6cd0e5ef65ca63d8
Fardin Mohammed <fardeenmohdpl@gmail.com>
2026-01-11 00:40:12 +0530

    Add DTOs for order, orderItem, orderRequest and payment

---
+++ src/main/java/com/webstore/payload/OrderItemDTO.java
@@ -0,0 +1,16 @@
+package com.webstore.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemDTO {
+    private Long id;
+    private ProductDTO product;
+    private Integer quantity;
+    private Double discount;
+    private Double orderedProductPrice;
+}
