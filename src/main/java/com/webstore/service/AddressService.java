49e011fc34de1cfa3c1bd5df47729cf599cd1517
Fardin Mohammed <fardeenmohdpl@gmail.com>
2026-01-10 23:51:48 +0530

    Add get addresses API

--- src/main/java/com/webstore/service/AddressService.java
+++ src/main/java/com/webstore/service/AddressService.java
@@ -3,7 +3,11 @@ package com.webstore.service;
 import com.webstore.model.User;
 import com.webstore.payload.AddressDTO;
 
+import java.util.List;
+
 public interface AddressService {
 
     AddressDTO createAddress(AddressDTO addressDTO, User user);
+
+    List<AddressDTO> getAddresses();
 }
