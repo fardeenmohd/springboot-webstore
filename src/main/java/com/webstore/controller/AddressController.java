447a17532a0bc0a8d602ad7995a71dbcce7f7a0c
Fardin Mohammed <fardeenmohdpl@gmail.com>
2026-01-10 23:34:00 +0530

    Fix add address request body

--- src/main/java/com/webstore/controller/AddressController.java
+++ src/main/java/com/webstore/controller/AddressController.java
@@ -4,10 +4,12 @@ import com.webstore.model.User;
 import com.webstore.payload.AddressDTO;
 import com.webstore.service.AddressService;
 import com.webstore.util.AuthUtil;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ public class AddressController {
     private AuthUtil authUtil;
 
     @PostMapping("/addresses")
-    public ResponseEntity<AddressDTO> createAddress(AddressDTO addressDTO) {
+    public ResponseEntity<AddressDTO> createAddress(@Valid @RequestBody AddressDTO addressDTO) {
         User user = authUtil.loggedInUser();
         AddressDTO savedAddressDTO = addressService.createAddress(addressDTO, user);
         return new ResponseEntity<>(savedAddressDTO, HttpStatus.CREATED);
