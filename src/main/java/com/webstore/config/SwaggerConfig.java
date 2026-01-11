c6ba7c7042280860209bbdb86ad4b078c59fafce
Fardin Mohammed <fardeenmohdpl@gmail.com>
2026-01-11 21:42:35 +0530

    Add custom spring boot links for swagger

--- src/main/java/com/webstore/config/SwaggerConfig.java
+++ src/main/java/com/webstore/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.webstore.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +24,20 @@ public class SwaggerConfig {
         SecurityRequirement bearerRequirement = new SecurityRequirement().addList("Bearer Authentication");
 
         return new OpenAPI()
+                .info(new Info().title("Spring Boot Web Store API")
+                        .version("1.0.0")
+                        .description("Spring Boot Web Store API")
+                        .license(new License().name("Apache 2.0"))
+                        .contact(new Contact().name("Fardin Mohammed")
+                                .email("fardeendmohdpl@gmail.com")
+                                .url("https://github.com/fardeenmohd/springboot-webstore"))
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("Spring Boot Web Store API Documentation")
+                        .url("https://github.com/fardeenmohd/springboot-webstore/blob/master/README.md")
+                )
                 .components(new Components()
-                        .addSecuritySchemes("Bearer Authentication", bearerScheme)).addSecurityItem(bearerRequirement);
+                        .addSecuritySchemes("Bearer Authentication", bearerScheme))
+                .addSecurityItem(bearerRequirement);
     }
 }
