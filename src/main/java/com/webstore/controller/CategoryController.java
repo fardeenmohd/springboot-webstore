a02138c721bc852158d8a75c14f694b89a908181
Fardin Mohammed <fardeenmohdpl@gmail.com>
2026-01-11 21:56:33 +0530

    Add swagger annotations to category controller and DTO

--- src/main/java/com/webstore/controller/CategoryController.java
+++ src/main/java/com/webstore/controller/CategoryController.java
@@ -4,6 +4,12 @@ import com.webstore.config.AppConstants;
 import com.webstore.payload.CategoryDTO;
 import com.webstore.payload.CategoryResponse;
 import com.webstore.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +22,7 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
             @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER) Integer pageNumber,
@@ -26,18 +33,27 @@ public class CategoryController {
         return ResponseEntity.ok(categoriesResponse);
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
     @PostMapping("/public/categories")
+    @Operation(summary = "Create category", description = "API to create new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Category created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
         CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
         return ResponseEntity.ok(savedCategoryDTO);
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId) {
+    public ResponseEntity<CategoryDTO> deleteCategory(@Parameter(description = "ID of the Category that you wish to delete") @PathVariable Long categoryId) {
         CategoryDTO deletedCategoryDTO = categoryService.deleteCategory(categoryId);
         return ResponseEntity.ok(deletedCategoryDTO);
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
     @PutMapping("/admin/categories")
     public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
         CategoryDTO updatedCategory = categoryService.updateCategory(categoryDTO);
--- src/main/java/com/webstore/payload/CategoryDTO.java
+++ src/main/java/com/webstore/payload/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.webstore.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryDTO {
+    @Schema(description = "Category ID", example = "111")
     private Long categoryId;
+    @Schema(description = "Category name for category you want to create", example = "iphone 14")
     private String categoryName;
 }
